19928967d7ae3c65a1907c77677e084e11123da1
AhmedKhchai <59939354+AhmedKhchai@users.noreply.github.com>
2022-01-12 10:37:07 +0100

    notes_category index

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\Note;
+use App\Entity\Category;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Form\NoteType;
 
@@ -16,23 +17,39 @@ class NoteController extends AbstractController
     /**
      * @Route("/", name="note_index")
      */
-    public function listNotes(ManagerRegistry $doctrine, Request $request): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
-      $rep = $doctrine->getRepository(Note::class);
-      return $this->render('note/index.html.twig', [
-        // 'notes' => $rep->findAll(),
-        'notes' => $rep->findBy(array(), array('created_at' => 'DESC')),
+        $repNote = $doctrine->getRepository(Note::class);
+        $repCategory = $doctrine->getRepository(Category::class);
+        return $this->render('note/index.html.twig', [
+        'categories' => $repCategory->findBy(array(), array('created_at' => 'DESC')),
+        'notes' => $repNote->findBy(array(), array('created_at' => 'DESC')),
+        
         'id' => $request->query->get('id')
+      ]);
+    }
+    /**
+     * @Route("/notes/category/{id}", name="note_category_index", requirements={"id"="\d+"})
+     */
+    public function index_cat(ManagerRegistry $doctrine, int $id)
+    {
+        $repCategory = $doctrine->getRepository(Category::class);
+        $category = $repCategory->find($id);
+        $notes = $category->getNotes();
+        return $this->render('note/index.html.twig', [
+        'categories' => $repCategory->findBy(array(), array('created_at' => 'DESC')),
+        'notes' => $notes,
       ]);
     }
 
     /**
       * @Route("/note/{id}", name="show_note", requirements={"id"="\d+"})
     */
-    public function showNote(ManagerRegistry $doctrine, int $id): Response {
-      $rep = $doctrine->getRepository(Note::class);
-      $note = $rep->find($id);
-      return $this->render('note/show_note.html.twig', [
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $rep = $doctrine->getRepository(Note::class);
+        $note = $rep->find($id);
+        return $this->render('note/show_note.html.twig', [
         'note' => $note
       ]);
     }
@@ -42,18 +59,18 @@ class NoteController extends AbstractController
      */
     public function create(ManagerRegistry $doctrine, Request $request): Response
     {
-      $note = new Note();
-      $form = $this->createForm(NoteType::class, $note);
+        $note = new Note();
+        $form = $this->createForm(NoteType::class, $note);
 
-      $form->handleRequest($request);
-      if ($form->isSubmitted() && $form->isValid()) {
-        $em = $doctrine->getManager();
-        $em->persist($note);
-        $em->flush();
-        return $this->redirectToRoute('note_index', ['id' => $note->getId()]);
-      }
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $em->persist($note);
+            $em->flush();
+            return $this->redirectToRoute('note_index', ['id' => $note->getId()]);
+        }
 
-      return $this->render('note/create.html.twig', [
+        return $this->render('note/create.html.twig', [
         'form' => $form->createView()
       ]);
     }
@@ -63,18 +80,18 @@ class NoteController extends AbstractController
      */
     public function edit(ManagerRegistry $doctrine, Request $request, int $id): Response
     {
-      $rep = $doctrine->getRepository(Note::class);
-      $note = $rep->find($id);
-      $form = $this->createForm(NoteType::class, $note);
+        $rep = $doctrine->getRepository(Note::class);
+        $note = $rep->find($id);
+        $form = $this->createForm(NoteType::class, $note);
 
-      $form->handleRequest($request);
-      if ($form->isSubmitted() && $form->isValid()) {
-        $em = $doctrine->getManager();
-        $em->flush();
-        return $this->redirectToRoute('note_index');
-      }
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $em->flush();
+            return $this->redirectToRoute('note_index');
+        }
 
-      return $this->render('note/create.html.twig', [
+        return $this->render('note/create.html.twig', [
         'form' => $form->createView()
       ]);
     }
@@ -84,13 +101,13 @@ class NoteController extends AbstractController
      */
     public function delete(ManagerRegistry $doctrine, Request $request, int $id): Response
     {
-      $rep = $doctrine->getRepository(Note::class);
-      $note = $rep->find($id);
-      $em = $doctrine->getManager();
-      $em->remove($note);
-      $em->flush();
+        $rep = $doctrine->getRepository(Note::class);
+        $note = $rep->find($id);
+        $em = $doctrine->getManager();
+        $em->remove($note);
+        $em->flush();
 
-      // enlever la redirection et déclencher une animation qui brûle la note
-      return $this->redirectToRoute('note_index');
+        // enlever la redirection et déclencher une animation qui brûle la note
+        return $this->redirectToRoute('note_index');
     }
 }
